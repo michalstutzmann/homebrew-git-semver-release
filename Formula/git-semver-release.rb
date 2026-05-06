00e03ca70411a77faa03681fd747d82ef810e639
Michał Stutzmann <michal.stutzmann@gmail.com>
2026-05-06 11:00:12 +0200

    Initial commit

---
+++ Formula/git-semver-release.rb
@@ -0,0 +1,18 @@
+class GitSemverRelease < Formula
+  desc "Semantic versioning tool for Git using conventional commits"
+  homepage "https://github.com/michalstutzmann/git-semver-release"
+  url "https://github.com/michalstutzmann/git-semver-release/releases/download/v1.0.0/git-semver-release"
+  sha256 "47d1ba04f7c5798d8541cf1c6bcc2cf867033ae13a9b6e239d63bac707a5420d"
+  license "Apache-2.0"
+
+  depends_on "bash"
+  depends_on "git"
+
+  def install
+    bin.install "git-semver-release"
+  end
+
+  test do
+    assert_match "not a git repository", shell_output("#{bin}/git-semver-release version 2>&1", 1)
+  end
+end
