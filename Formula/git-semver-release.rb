24b4f5fed5ca34549c742f09c79dac1994775e7f
Michał Stutzmann <michal.stutzmann@gmail.com>
2026-05-31 08:03:06 +0200

    Bump git-semver-release to 2.0.1

--- Formula/git-semver-release.rb
+++ Formula/git-semver-release.rb
@@ -1,8 +1,8 @@
 class GitSemverRelease < Formula
   desc "Semantic versioning tool for Git using conventional commits"
   homepage "https://github.com/michalstutzmann/git-semver-release"
-  url "https://github.com/michalstutzmann/git-semver-release/releases/download/v2.0.0/git-semver-release"
-  sha256 "8f87b8e1c5b3eed828a82f30489f82b76d6f8919a0602722841351a4b152c95d"
+  url "https://github.com/michalstutzmann/git-semver-release/releases/download/v2.0.1/git-semver-release"
+  sha256 "9d15c0e767a1948e6f5bf9697030c7e47b2773d4a4f66264a63f2b0922a5f027"
   license "Apache-2.0"
 
   depends_on "bash"
